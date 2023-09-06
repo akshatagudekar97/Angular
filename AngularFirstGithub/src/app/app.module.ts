388b245c63a707ccc3ccbb08c545970ff844159b
akshatagudekar97 <akshatagudekar97@gmail.com>
2023-09-06 21:57:16 +0530

    All Is Project Done

---
+++ AngularFirstGithub/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './component/home/home.component';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavabarComponent } from './component/navabar/navabar.component';
+import {MatIconModule} from '@angular/material/icon';
+import { EmailComponent } from './component/email/email.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavabarComponent,
+    EmailComponent,
+  
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  providers: [MatSnackBar],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
